4587a8a23ee6edcd9dce02aecbd9ce87902ac7d8
Palelo <pablo.caraballorente@gmail.com>
2018-10-22 11:14:19 +0200

    All done.

--- Instalapache.py
+++ Instalapache.py
@@ -1,51 +1,71 @@
 import os
-import sys
 
 # Configuracion de la interfaz de red
-interfaces = open('/etc/network/interfaces', 'w')
+os.system('chmod 777 /etc/network/interfaces')
+interfaces = open('/etc/network/interfaces', 'r')
+interfacesdos = open('/etc/network/interfaces2', 'w')
 
 for line in interfaces:
     if 'source /etc/network/interfaces.d/*.cfg' in line:
-        interfaces.write('auto eth0\n iface eth0 inet static\n address 192.168.122.241\n nermask 255.255.255.0\n'
-                         'gateway 192.168.122.1\n dns-nameservers 192.168.122.1')
+        interfacesdos.write('auto eth0\n'
+                            'iface eth0 inet static\n'
+                            'address 192.168.122.241\n'
+                            'nermask 255.255.255.0\n'
+                            'gateway 192.168.122.1\n '
+                            'dns-nameservers 192.168.122.1')
     else:
-        interfaces.write(line)
+        interfacesdos.write(line)
 
-#Instalacion de apache con todos sus extras para el correcto funcionamiento
-os.system("sudo apt-get install apache2")
-os.system("sudo apt-get install lynx")
-os.system("sudo apt-get install wget")
-os.system("sudo apt-get install curl")
+interfaces.close()
+interfacesdos.close()
+
+os.system('sudo mv /etc/network/interfaces2 /etc/network/interfaces | chmod 777 /etc/network/interfaces')
+
+# Instalacion de apache con todos sus extras para el correcto funcionamiento
+os.system("sudo apt-get install -y apache2")
+os.system("sudo apt-get install -y lynx")
+os.system("sudo apt-get install -y wget")
+os.system("sudo apt-get install -y curl")
 os.system("sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/dominio1.conf")
 os.system("sudo chmod 777 /etc/apache2/sites-available/dominio1.conf")
 
-#Anadir ServerName dominio1.cdps y ServerAlias www.dominio1.cdps
-f1 = open("/etc/apache2/sites-available/dominio1.conf", "w")
+# Anadir ServerName dominio1.cdps y ServerAlias www.dominio1.cdps
+f1 = open("/etc/apache2/sites-available/dominio1.conf", "r")
+f2 = open("/etc/apache2/sites-available/dominio1.conf.b", "w+")
+
 for line in f1:
-        if "DocumentRoot /var/www/dom1" in line:
-            f1.write(line)
-            f1.write("ServerName dominio1.cdps\nServerAlias www.dominio1.cdps\n")
-        else:
-                f1.write(line)
+    if "DocumentRoot /var/www/html" in line:
+        f2.write('DocumentRoot /var/www/dom1\n')
+        f2.write("ServerName dominio1.cdps\nServerAlias www.dominio1.cdps\n")
+    else:
+        f2.write(line)
+
 f1.close()
+f2.close()
 
-#Recarga de apache con los cambios realizados
+os.system('sudo mv /etc/apache2/sites-available/dominio1.conf.b /etc/apache2/sites-available/dominio1.conf')
+
+# Recarga de apache con los cambios realizados
 os.system("sudo a2ensite dominio1.conf")
-os.system("sudo apache2 reload")
+os.system("sudo service apache2 reload")
 
-#Mismo proceso para la segunda pagina web
+# Mismo proceso para la segunda pagina web
 fileDom1 = open("/etc/apache2/sites-available/dominio1.conf", 'r') 
-fileDom2 = open("/etc/apache2/sites-available/dominio2.conf", 'w') 
+fileDom2 = open("/etc/apache2/sites-available/dominio2.conf", 'w')
+
 for line in fileDom1:
-   if "ServerName dominio1.cdps" in line :
-       fileDom2.write("ServerName dominio2.cdps \n")
-   elif "ServerAlias www.dominio1.cdps" in line :
-       fileDom2.write("ServerAlias www.dominio2.cdps \n")
-   else:
-      fileDom2.write(line)
+    if "DocumentRoot /var/www/dom1" in line:
+        fileDom2.write('DocumentRoot /var/www/dom2\n')
+    elif "ServerName dominio1.cdps" in line :
+        fileDom2.write("ServerName dominio2.cdps \n")
+    elif "ServerAlias www.dominio1.cdps" in line :
+        fileDom2.write("ServerAlias www.dominio2.cdps \n")
+    else:
+        fileDom2.write(line)
+
 fileDom1.close()
 fileDom2.close()
 
-#Recarga de apache con los cambios realizados
+# Recarga de apache con los cambios realizados
 os.system("sudo a2ensite dominio2.conf")
 os.system("sudo service apache2 reload")
--- vms.py
+++ vms.py
@@ -1,7 +1,5 @@
 from subprocess import call
-import time
 import subprocess
-import kvm
 import os
 
 # REQUISITOS
@@ -20,26 +18,31 @@ print 'Generando imagen .qcow2...'
 call('qemu-img convert -O qcow2 cdps-vm-base-p1.img cdps-vm-base-p1.qcow2', shell=True)
 call('chmod 666 cdps-vm-base-p1.qcow2', shell=True)
 
-# Creamos la vm
-print 'Instalando maquina virtual base...'
-call('sudo virt-install --import --name base --memory 2048 --disk cdps-vm-base-p1.qcow2 --vcpus=2 --os-type linux \\'
-     '--network default --keymap es --connect qemu:///system --noautoconsole', shell=True)
+# DESCOMENTAR EL SIGUIENTE BLOQUE PARA USO CON KVM + MULTIPLES VMS
 
-# Obtenemos el ID de la vm
-print 'Obteniendo ID de la vm para dumpear su XML...'
-vmId = subprocess.check_output("sudo virsh list --all | grep 'base' | awk  '{ print $1 }'", shell=True)
-
-# Dumpeamos el XML de la vm
-print 'Dumpeando XML base para poder crear n copias OW'
-xml = subprocess.check_output("sudo virsh dumpxml %s" % vmId, shell=True)
-newXml = open('cdps-vm-base-p1.xml', "w+")
-newXml.write(xml)
-newXml.close()
-
-# Apagamos la base
-time.sleep(10)
-call('virsh shutdown base', shell=True)
-time.sleep(2)
+# import kvm
+# import time
+#
+# # Creamos la vm
+# print 'Instalando maquina virtual base...'
+# call('virt-install --import --name base --memory 2048 --disk cdps-vm-base-p1.qcow2 --vcpus=2 --os-type linux \\'
+#      '--network default --keymap es --connect qemu:///system --noautoconsole', shell=True)
+#
+# # Obtenemos el ID de la vm
+# print 'Obteniendo ID de la vm para dumpear su XML...'
+# vmId = subprocess.check_output("sudo virsh list --all | grep 'base' | awk  '{ print $1 }'", shell=True)
+#
+# # Dumpeamos el XML de la vm
+# print 'Dumpeando XML base para poder crear n copias OW'
+# xml = subprocess.check_output("sudo virsh dumpxml %s" % vmId, shell=True)
+# newXml = open('cdps-vm-base-p1.xml', "w+")
+# newXml.write(xml)
+# newXml.close()
+#
+# # Apagamos la base
+# time.sleep(10)
+# call('virsh shutdown base', shell=True)
+# time.sleep(2)
 
 n = input('Numero de maquinas a crear: ')
 
@@ -51,13 +54,20 @@ for i in range(0, n):
     # Generamos .qcow2 para cada vm
     print 'Generando base (COW) de la VM', i
     qcow = '%s.qcow2' % vmName
-    s = 'sudo qemu-img create -f qcow2 -b cdps-vm-base-p1.qcow2 %s' % qcow
+    s = 'qemu-img create -f qcow2 -b cdps-vm-base-p1.qcow2 %s' % qcow
     call(s, shell=True)
 
     # Generamos su source, UUID y MAC
     pwd = '%s/%s' % (os.getcwd(), qcow)
-    uuid = kvm.gen_uuid()
-    mac = kvm.gen_mac()
+
+    # DESCOMENTAR PARA USO CON KVM + MULTIPLES VMS
+    # uuid = kvm.gen_uuid()
+    # mac = kvm.gen_mac()
+
+    # COMENTAR PARA USO SIN KVM + UNA VM
+    uuid = '85d78af1-6fe6-2851-9f38-600c047d25a8'
+    mac = '54:52:00:44:c3:0f'
+
     print 'Datos de la VM %s' % vmName
     print 'SRC: %s' % pwd
     print 'UUID: %s' % uuid
@@ -87,38 +97,8 @@ for i in range(0, n):
     print 'Creando la vm...'
     call('sudo virsh create %s' % xml, shell=True)
 
-    # # Configuracion de red de la maquina
-    # print 'Configurando la vm...'
-    # time.sleep(10)
-    #
-    # # Sacamos la IP
-    # ip = subprocess.check_output("sudo virsh domifaddr %s | grep ipv4 | awk '{print $4}' | cut -d '/' -f 1" % vmName,
-    #                              shell=True)
-    #
-    # # Generamos el .cfg de la interfaces
-    # interfaces = open('50-cloud-init.cfg.%i' % i, 'w')
-    #
-    # addr = 100 + i
-    #
-    # interfaces.write('auto lo\n')
-    # interfaces.write('iface lo inet loopback\n')
-    # interfaces.write('\n')
-    # interfaces.write('auto eth0\n')
-    # interfaces.write('iface eth0 inet static\n')
-    # interfaces.write('address 192.168.122.%i\n' % addr)
-    # interfaces.write('netmask 255.255.255.0\n')
-    # interfaces.write('gateway 192.168.122.1\n')
-    # interfaces.write('dns-nameservers 192.168.122.1\n')
-    #
-    # interfaces.close()
-    #
-    # # Movemos el .cfg a la VM y la reiniciamos para que se haga efectivo
-    # p = subprocess.Popen(['scp', '50-cloud-init.cfg.%i' % i,
-    #                       'cdps@%s:/etc/network/interfaces.d/50-cloud-init.cfg' % ip])
-    # sts = os.waitpid(p.pid, 0)
-    # call('sudo virsh reboot %s' % vmName, shell=True)
-    #
-    # # Limpiamos el directorio
-    # call('rm 50-cloud-init.cfg.%i')
-    # call('rm %s' % xml)
-    print 'VM %s configurada.' % vmName
+    # Sacamos la IP
+    ip = subprocess.check_output("sudo virsh domifaddr %s | grep ipv4 | awk '{print $4}' | cut -d '/' -f 1" % vmName,
+                                 shell=True)
+
+    print 'VM %s configurada @  %s' % (vmName, ip)
